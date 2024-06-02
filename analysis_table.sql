18ecd0f3463a919eb0ca78b80eb84496bcdefc75
Rosyiidah Dhiya'Ulhaq <117335948+dhiyross@users.noreply.github.com>
2024-06-03 01:53:28 +0700

    Update analysis_table.sql

--- analysis_table.sql
+++ analysis_table.sql
@@ -1,39 +1,39 @@
 CREATE TABLE rakamin-kf-analytics-01.kimia_farma.tabel_analisa AS
 SELECT
-    t.transaction_id,
-    t.date,
-    b.branch_id,
-    b.branch_name,
-    b.kota,
-    b.provinsi,
-    b.rating_cabang,
-    t.customer_name,
-    p.product_id,
-    p.product_name,
-    p.price,
-    t.discount_percentage,
+    FT.transaction_id,
+    FT.date,
+    KC.branch_id,
+    KC.branch_name,
+    KC.kota,
+    KC.provinsi,
+    KC.rating AS rating_cabang,
+    FT.customer_name,
+    P.product_id,
+    P.product_name,
+    P.price,
+    FT.discount_percentage,
     CASE
-        WHEN p.price <= 50000 THEN 0.10
-        WHEN p.price > 50000 AND p.price <= 100000 THEN 0.15
-        WHEN p.price > 100000 AND p.price <= 300000 THEN 0.20
-        WHEN p.price > 300000 AND p.price <= 500000 THEN 0.25
+        WHEN P.price <= 50000 THEN 0.10
+        WHEN P.price > 50000 AND P.price <= 100000 THEN 0.15
+        WHEN P.price > 100000 AND P.price <= 300000 THEN 0.20
+        WHEN P.price > 300000 AND P.price <= 500000 THEN 0.25
         ELSE 0.30
     END AS persentase_gross_laba,
-    (p.price * (1 - t.discount_percentage / 100)) AS nett_sales,
-    (p.price * (1 - t.discount_percentage / 100)) * 
+    (P.price * (1 - FT.discount_percentage)) AS nett_sales,
+    (P.price * (1 - FT.discount_percentage)) * 
     CASE
-        WHEN p.price <= 50000 THEN 0.10
-        WHEN p.price > 50000 AND p.price <= 100000 THEN 0.15
-        WHEN p.price > 100000 AND p.price <= 300000 THEN 0.20
-        WHEN p.price > 300000 AND p.price <= 500000 THEN 0.25
+        WHEN P.price <= 50000 THEN 0.10
+        WHEN P.price > 50000 AND P.price <= 100000 THEN 0.15
+        WHEN P.price > 100000 AND P.price <= 300000 THEN 0.20
+        WHEN P.price > 300000 AND P.price <= 500000 THEN 0.25
         ELSE 0.30
     END AS nett_profit,
-    t.rating_transaksi
+    FT.rating AS rating_transaksi
 FROM
-    rakamin-kf-analytics-01.kimia_farma.kf_final_transaction t
+    rakamin-kf-analytics-01.kimia_farma.kf_final_transaction FT
 JOIN
-    rakamin-kf-analytics-01.kimia_farma.kf_kantor_cabang b ON t.branch_id = b.branch_id
+    rakamin-kf-analytics-01.kimia_farma.kf_kantor_cabang KC ON FT.branch_id = KC.branch_id
 JOIN
-    rakamin-kf-analytics-01.kimia_farma.kf_inventory c ON t.product_id = c.product_id
+    rakamin-kf-analytics-01.kimia_farma.kf_inventory I ON FT.product_id = I.product_id
 JOIN
-    rakamin-kf-analytics-01.kimia_farma.kf_product p ON t.product_id = p.product_id
+    rakamin-kf-analytics-01.kimia_farma.kf_product P ON FT.product_id = P.product_id
